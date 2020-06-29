8afe910d11d96d4b67b2d24d04f067983b763b63
Yasuhiro Yamada <yamadagrep@gmail.com>
2020-06-29 22:33:32 +0100

    Add version to super_unko.rb

--- super_unko.rb
+++ super_unko.rb
@@ -1,5 +1,6 @@
 class SuperUnko < Formula
   desc "Let's us create shit commands"
+  version "1.0.5"
   homepage "https://github.com/greymd/super_unko"
   url "https://github.com/unkontributors/super_unko.git", :branch => "oshiri"
   head "https://github.com/unkontributors/super_unko.git", :branch => "oshiri"
